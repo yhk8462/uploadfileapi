bd2ade9039d618c71db36b3468d0f46912673005
yhk8462 <s3726115@rmit.edu.vn>
2020-01-07 23:02:50 +0700

    f

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+var multer = require('multer')
+
+var myStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    }
+    ,
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+
+
+var uploader = multer({ storage: myStorage })
+
+
+app.post('/uploadfile', uploader.single('myFile'), function (req, res, next) {
+
+    const file = req.file
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+
+    res.send(req.file)
+})
+
+
+app.listen(3003)
